ee8186bbd7457a714a7b11eb60f09c7827d4c5e9
Wander Souza <wanderrodrigues_@outlook.com>
2021-07-25 15:49:13 -0300

    :sunflower: myplant

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,21 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
     StyleSheet,
     View,
     Text,
-    Image
+    Image,
+    FlatList
 } from 'react-native';
 
 import { Header } from "../components/Header";
+
+import waterdrop from '../assets/waterdrop.png';
 import colors from "../styles/colors";
 
+import { loadPlant, PlantProps } from "../libs/storage";
+import { formatDistance } from "date-fns";
+import pt from "date-fns/esm/locale/pt/index.js";
+import fonts from "../styles/fonts";
+
 export function MyPlants() {
+
+    const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
+    const [loading, setLoading] = useState (true);
+    const [ nextWaterd, setNextWatered] = useState<string>();
+
+    useEffect(() => {
+        async function loadStorageData() {
+            const plantsStoraged = await loadPlant();
+
+            const nexTime = formatDistance(
+                new Date(plantsStoraged[0].dateTimeNotification).getTime(),
+                new Date().getTime(),
+                { locale: pt }
+            );
+
+            setNextWatered(
+                `Não esqueça de regar a ${plantsStoraged[0].name} à ${nexTime} horas.`
+            )
+
+            setMyPlants(plantsStoraged);
+            setLoading(false);
+        }
+
+        loadStorageData();
+    },[])
+
     return (
         <View style={styles.container}>
             <Header/>
 
             <View style={styles.spotlight}>
-                <Image source={}/>
+                <Image 
+                source={waterdrop}
+                style={styles.spotlightImage}
+                />
+            </View>
+            <Text style={styles.spotlightText}>
+                {nextWaterd}
+            </Text>
+
+            <View style={styles.plants}>
+                <Text style={styles.plantsTitle}>
+                Proximas regadas
+                </Text>
+                
+
+                <FlatList
+                    data={myPlants}
+                    keyExtractor= {(item)=> String(item.id)}
+                    renderItem={({ item }) =>(
+                        <Text>Elemento</Text>
+                    )}
+                />
             </View>
 
         </View>
@@ -30,5 +85,39 @@ const styles = StyleSheet.create({
         paddingHorizontal: 30,
         paddingTop: 50,
         backgroundColor: colors.background
+    },
+
+    spotlight: {
+        backgroundColor: colors.blue_light,
+        paddingHorizontal: 20,
+        bordeRadius: 20,
+        height: 110,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    
+    spotlightImage: {
+        width: 60,
+        height: 60
+    },
+
+    spotlightText: {
+        flex: 1,
+        color: colors.blue,
+        paddingHorizontal: 20,
+        textAlign: 'justify'
+    },
+
+    plants: {
+        flex: 1,
+        width: '100%'
+    },
+
+    plantsTitle: {
+        fontSize: 24,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        marginVertical: 20
     }
 })
